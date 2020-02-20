97a9ce00d5d5b49bf1142ac901944131d708ff27
Olvea16 <olvea16@student.sdu.dk>
2020-02-20 08:17:37 +0100

    raiders doesnt include header row

--- raiders.py
+++ raiders.py
@@ -83,18 +83,19 @@ def update_raiders():
     for i, row in enumerate(sheet_values[1:]):
         while (i + offset in del_rows or i + offset is 1): offset += 1 
         
-        cells = {sheet_values[header_row][j]:row[j] for j in range(0, len(row))}
+        if not set(['name', 'class', 'role']) <= set(row):
+            cells = {sheet_values[header_row][j]:row[j] for j in range(0, len(row))}
 
-        raider =  {}
-        raider['row'] = i + offset + 1 # 1-indexed
-        raider['class'] = cells['class']
-        raider['role'] = cells['role']
-        raider['team'] = cells['team']
-        raider['attendance'] = cells['attendance']
+            raider =  {}
+            raider['row'] = i + offset + 1 # 1-indexed
+            raider['class'] = cells['class']
+            raider['role'] = cells['role']
+            raider['team'] = cells['team']
+            raider['attendance'] = cells['attendance']
 
-        raider['performance'] = {key:cells[key] for key in ['bwl', 'mc', 'ony']}
+            raider['performance'] = {key:cells[key] for key in ['bwl', 'mc', 'ony']}
 
-        raiders[cells['name']] = raider
+            raiders[cells['name']] = raider
 
     prev_raiders = list(raiders_file.get('raiders', on_error={}).keys())
     raiders_file.set('raiders', raiders)
